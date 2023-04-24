69bfedebf6e1d6f47c9f3f12d30c579c991449c9
kittypark <kittypark.github.io@gmail.com>
2023-04-24 16:51:58 +0900

    test

--- wordle/script.js
+++ wordle/script.js
@@ -109,9 +109,9 @@ var template =
 document.getElementById('rule').addEventListener('click', function () {
     document.querySelector('.modal').style.display = 'flex';
 }
-);
+)
 document.querySelector('.close').addEventListener('click', function () {
     document.querySelector('.modal').style.display = 'none';
 }
 
-);
+)
