0bd2566cf48bd6c21d60db2734d1d684092abd02
kittypark <kittypark.github.io@gmail.com>
2023-04-25 14:40:51 +0900

    recommit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function goToWordle() {
     window.location.href = "wordle/index.html";
 }
+
 function goToPhoto() {
     window.location.href = "photo_toy/index.html";
 }
+
 function goToCoin() {
     window.location.href = "rotatecoin/index.html";
 }
