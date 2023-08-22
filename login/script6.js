94c40c252df8476fb2eb91c872e7d5c54b4fa6c9
kittypark <kittypark.github.io@gmail.com>
2023-08-22 16:50:41 +0900

    docs : domain update

--- login/script6.js
+++ login/script6.js
@@ -25,10 +25,12 @@ function login() {
     var data = { "username": id, "password": hash_pw };
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", `https://toy.rainclouds.xyz/login?username=${id}&password=${hash_pw}`);
+    // data 를 payload 로 보내는 것이 아니라, url 에 붙여서 보내는 것이다.
+    xhr.open("GET", `https://api.rainclouds.xyz/login?username=${id}&password=${hash_pw}`);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     xhr.send(JSON.stringify(data));
+
 
     xhr.addEventListener("load", function () {
         var result = JSON.parse(xhr.responseText);
@@ -55,8 +57,7 @@ function signup() {
     const hash_pw1 = CryptoJS.SHA256(pw1).toString();
     var data = { "username": id1, "password": hash_pw1, "phone": phone, "name": name };
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", `https://toy.rainclouds.xyz/signup?username=${id1}&password=${hash_pw1}&phone=${phone}&name=${name}`);
-    // xhr.open("POST", "https://toy.rainclouds.xyz:8443/signup?username=" + id1 + "&password=" + hash_pw1 + "&phone=" + phone + "&name=" + name);
+    xhr.open("POST", `https://api.rainclouds.xyz/signup?username=${id1}&password=${hash_pw1}&phone=${phone}&name=${name}`);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     xhr.send(JSON.stringify(data));
